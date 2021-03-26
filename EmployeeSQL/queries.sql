bfcdf2444f4949b19281e3d235e889488be9a4e7
Judith Samuels <fromjandp@aol.com>
2021-03-25 22:31:57 -0500

    Seventh Query

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -16,7 +16,7 @@ ORDER BY e.emp_no;
 
 SELECT first_name, 
        last_name, 
-	   hire_date
+       hire_date
 FROM employees
 WHERE hire_date 
     BETWEEN '1986-01-01' AND '1986-12-31';
@@ -36,7 +36,7 @@ FROM dept_manager AS dm
         ON (dm.dept_no = d.dept_no)
     INNER JOIN employees AS e
         ON (dm.emp_no = e.emp_no);
-        
+
 
 -- 4. List the department of each employee with the following information:
 --      employee number, last name, first name, department name
@@ -60,13 +60,14 @@ ORDER BY e.emp_no;
 
 SELECT first_name, 
        last_name,
-	   sex
+       sex
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 
 -- 6. List all employees in the Sales department, including their employee number, 
 --      last name, first name, department name    
+--      (This code is using 'e' for the employees Table, 'd' : departments Table, de : dept_emp Table)
 
         
 SELECT  e.emp_no,
@@ -82,8 +83,44 @@ WHERE d.dept_name = 'Sales'
 ORDER BY e.emp_no;
 
 
+-- 7. List all employees in the Sales and Development departments, including their 
+--      employee number, last name, first name, department name
 
 
-        
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+           INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development')
+ORDER BY e.emp_no;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN employees AS e
+        ON (dm.emp_no = e.emp_no);    
+    
+    
+    
 
 
